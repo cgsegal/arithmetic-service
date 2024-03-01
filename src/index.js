c493431121028a6ee1f101143498b0070350a6b5
cgsegal <charliegriffinsegal@gmail.com>
2024-02-29 21:49:21 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,36 @@ app.get('/', (req, res) => {
     res.send('Arithmetic service - last updated 3:35pm 2/15');
 });
 
+// addition
 app.get('/add/:n/:m', (req, res) => {
     let n = Number(req.params.n);
     let m = Number(req.params.m);
     res.json(sum);
 })
+
+// subtraction
+app.get('/subtract/:n/:m', (req, res) => {
+    let n = Number(req.params.n);
+    let m = Number(req.params.m);
+    let result = subtract(n, m);
+    res.json(result);
+});
+
+// multiplication
+app.get('/multiply/:n/:m', (req, res) => {
+    let n = Number(req.params.n);
+    let m = Number(req.params.m);
+    let result = multiply(n, m);
+    res.json(result);
+});
+
+// division
+app.get('/divide/:n/:m', (req, res) => {
+    let n = Number(req.params.n);
+    let m = Number(req.params.m);
+    let result = divide(n, m);
+    res.json(result);
+});
+
 
 app.listen(port);
